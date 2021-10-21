701aa69d34577aba2aa247e469197c986052eeee
lukePetersen9 <lukepetersen29@gmail.com>
2021-10-21 14:56:54 -0400

    massive updates

--- redux/actions/food-action.js
+++
@@ -1,8 +0,0 @@
-export const SET_MEALS = "SET_MEALS"
-
-export const setMeals = meals => (
-    {
-        type: SET_MEALS,
-        payload: meals,
-    }
-);
--- redux/actions/user-action.js
+++
@@ -1,8 +0,0 @@
-export const SET_LOCATION = "SET_LOCATION"
-
-export const updateLocation = location => (
-    {
-        type: SET_LOCATION,
-        payload: location,
-    }
-);
